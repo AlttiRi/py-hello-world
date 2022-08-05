e3772db570e889a3ba9619902d191b4b19760a5f
AlttiRi <16310547+AlttiRi@users.noreply.github.com>
2022-08-05 03:52:04 +0300

    Update

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ setup(
     version="0.0.1",
     entry_points={
         "console_scripts": [
-            "py-hello-world = main:say_hi",
-            "py-hello-world-ni = main_no_import:say_hi",
+            "py-hello-world     = main:say_hi",
+            "py-hello-world-ni  = main_no_import:say_hi",
             "py-hello-world-sub = hello_world.main_sub:say_hi"
         ]
     }
