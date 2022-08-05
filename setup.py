efefad1ee5d0130a4b8a5b037a00520eead7d737
AlttiRi <16310547+AlttiRi@users.noreply.github.com>
2022-08-05 04:12:09 +0300

    Update

--- setup.py
+++ setup.py
@@ -2,16 +2,16 @@ from setuptools import setup
 
 setup(
     name="py-hello-world",
-    version="0.0.5",
+    version="0.0.6",
     packages=[
         "hello_world",
         "hello_world.util",
     ],
     entry_points={
         "console_scripts": [
-            "py-hello-world     = main:say_hi",
-            "py-hello-world-ni  = main_no_import:say_hi",
-            "py-hello-world-sub = hello_world.main_sub:say_hi",
+            "py-hello-world     = hello_world.main:say_hi",
+            "py-hello-world-ni  = hello_world.main_no_import:say_hi",
+            "py-hello-world-sub = hello_world.util.main_sub:say_hi",
         ]
     }
 )
