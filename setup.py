44a6039ad06586bf1a36a498878f9ddda929f758
AlttiRi <16310547+AlttiRi@users.noreply.github.com>
2022-08-05 05:12:58 +0300

    Update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup  # find_packages
 
 setup(
     name="py-hello-world",
-    version="0.0.15",
+    version="0.0.16",
     packages=[
         "hello_world",
         "hello_world.util",
