04aed12f8c29d52a30b062c5cd418c9cfa01c619
AlttiRi <16310547+AlttiRi@users.noreply.github.com>
2022-08-05 04:16:28 +0300

    Update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="py-hello-world",
-    version="0.0.6",
+    version="0.0.7",
     packages=find_packages(),
     entry_points={
         "console_scripts": [
