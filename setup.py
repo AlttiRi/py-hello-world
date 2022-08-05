651b27f4cfe476b10ebb16409fa5ee08dbc152b5
AlttiRi <16310547+AlttiRi@users.noreply.github.com>
2022-08-05 05:25:46 +0300

    Update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup  # find_packages
 
 setup(
     name="py-hello-world",
-    version="0.0.21",
+    version="0.0.22",
     packages=[
         "hello_world",
         "hello_world.util",
@@ -13,6 +13,5 @@ setup(
             "py-hello-world-i   = hello_world.main_import:say_hi",
             "py-hello-world-sub = hello_world.util.main_sub:say_hi",
         ]
-    },
-    data_files=[("./xxx", ["../README.md"])],
+    }
 )
