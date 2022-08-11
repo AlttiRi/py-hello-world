2508360a1b521bd56c66c11d1e6ada88a92e2a2c
[Alt'tiRi] <16310547+AlttiRi@users.noreply.github.com>
2022-08-11 04:26:10 +0300

    Update __main__.py

--- hello_world/__main__.py
+++ hello_world/__main__.py
@@ -7,7 +7,7 @@ from hello_world.command_line_args import trace
 def say_hi():
     if trace:
         print("[trace]: __main__.py:say_hi()")
-    print(colored("Hello World!123", color="green", attrs=["bold"]))
+    print(colored("Hello World!123456", color="green", attrs=["bold"]))
 
 
 if __name__ == "__main__":
